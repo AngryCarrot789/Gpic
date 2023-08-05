4b2c82c254f9ec5424f6aba7fbdb8d5178bb7be3
REghZy <kettlesimzulator420@gmail.com>
2023-08-05 09:49:19 +0100

    Added a colour picker (PixiEditor/ColorPicker) to replace the windows forms one. I couldn't get the nuget package to work so I added the project to this solution. Click one of the colours on the bottom to show a dialog. Enter to confirm, esc to cancel

---
+++ ColorPicker/UIExtensions/HsvColorSlider.cs
@@ -0,0 +1,71 @@
+﻿using ColorPicker.Models;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ColorPicker.UIExtensions {
+    internal class HsvColorSlider : PreviewColorSlider {
+        public static readonly DependencyProperty SliderHsvTypeProperty =
+            DependencyProperty.Register(nameof(SliderHsvType), typeof(string), typeof(HsvColorSlider),
+                new PropertyMetadata(""));
+
+        protected override bool RefreshGradient => this.SliderHsvType != "H";
+
+        public HsvColorSlider() : base() { }
+
+        public string SliderHsvType {
+            get => (string) this.GetValue(SliderHsvTypeProperty);
+            set => this.SetValue(SliderHsvTypeProperty, value);
+        }
+
+        protected override void GenerateBackground() {
+            if (this.SliderHsvType == "H") {
+                var colorStart = this.GetColorForSelectedArgb(0);
+                var colorEnd = this.GetColorForSelectedArgb(360);
+                this.LeftCapColor.Color = colorStart;
+                this.RightCapColor.Color = colorEnd;
+                this.BackgroundGradient = new GradientStopCollection() {
+                    new GradientStop(colorStart, 0),
+                    new GradientStop(this.GetColorForSelectedArgb(60), 1 / 6.0),
+                    new GradientStop(this.GetColorForSelectedArgb(120), 2 / 6.0),
+                    new GradientStop(this.GetColorForSelectedArgb(180), 0.5),
+                    new GradientStop(this.GetColorForSelectedArgb(240), 4 / 6.0),
+                    new GradientStop(this.GetColorForSelectedArgb(300), 5 / 6.0),
+                    new GradientStop(colorEnd, 1)
+                };
+                return;
+            }
+
+            {
+                var colorStart = this.GetColorForSelectedArgb(0);
+                var colorEnd = this.GetColorForSelectedArgb(255);
+                this.LeftCapColor.Color = colorStart;
+                this.RightCapColor.Color = colorEnd;
+                this.BackgroundGradient = new GradientStopCollection {
+                    new GradientStop(colorStart, 0.0),
+                    new GradientStop(colorEnd, 1)
+                };
+            }
+        }
+
+        private Color GetColorForSelectedArgb(int value) {
+            switch (this.SliderHsvType) {
+                case "H": {
+                    var rgbtuple = ColorSpaceHelper.HsvToRgb(value, 1.0, 1.0);
+                    double r = rgbtuple.Item1, g = rgbtuple.Item2, b = rgbtuple.Item3;
+                    return Color.FromArgb(255, (byte) (r * 255), (byte) (g * 255), (byte) (b * 255));
+                }
+                case "S": {
+                    var rgbtuple = ColorSpaceHelper.HsvToRgb(this.CurrentColorState.HSV_H, value / 255.0, this.CurrentColorState.HSV_V);
+                    double r = rgbtuple.Item1, g = rgbtuple.Item2, b = rgbtuple.Item3;
+                    return Color.FromArgb(255, (byte) (r * 255), (byte) (g * 255), (byte) (b * 255));
+                }
+                case "V": {
+                    var rgbtuple = ColorSpaceHelper.HsvToRgb(this.CurrentColorState.HSV_H, this.CurrentColorState.HSV_S, value / 255.0);
+                    double r = rgbtuple.Item1, g = rgbtuple.Item2, b = rgbtuple.Item3;
+                    return Color.FromArgb(255, (byte) (r * 255), (byte) (g * 255), (byte) (b * 255));
+                }
+                default: return Color.FromArgb((byte) (this.CurrentColorState.A * 255), (byte) (this.CurrentColorState.RGB_R * 255), (byte) (this.CurrentColorState.RGB_G * 255), (byte) (this.CurrentColorState.RGB_B * 255));
+            }
+        }
+    }
+}
--- Gpic/Services/ColourPickerService.cs
+++ Gpic/Services/ColourPickerService.cs
@@ -1,31 +1,51 @@
-using System.Drawing;
-using System.Windows.Forms;
+using System.Windows;
+using Gpic.Controls;
 using Gpic.Core;
 using Gpic.Core.Services;
+using Gpic.Services.ColourPickers;
 using SkiaSharp;
 
 namespace Gpic.Services {
     [ServiceImplementation(typeof(IColourPicker))]
     public class ColourPickerService : IColourPicker {
         public SKColor? PickARGB(SKColor? def = null) {
-            ColorDialog dialog = new ColorDialog() {
-                AnyColor = true,
-                SolidColorOnly = false,
-                AllowFullOpen = true,
-                FullOpen = true
+            ColourPickerWindow window = new ColourPickerWindow {
+                WindowStyle = WindowStyle.None,
+                WindowStartupLocation = WindowStartupLocation.Manual,
+                Height = 426
             };
 
-            if (def is SKColor d) {
-                dialog.Color = Color.FromArgb(d.Alpha, d.Red, d.Green, d.Blue);
+            if (def is SKColor colour) {
+                window.Colour = colour;
             }
 
-            if (dialog.ShowDialog() == DialogResult.OK) {
-                Color c = dialog.Color;
-                return new SKColor(c.R, c.G, c.B, c.A);
+            CursorUtils.POINT pos = CursorUtils.GetCursorPos();
+            window.Left = pos.x;
+            window.Top = pos.y - 426;
+
+            if (window.ShowDialog() == true) {
+                return window.ActiveColour;
             }
 
             return null;
-        }
 
+            // ColorDialog dialog = new ColorDialog() {
+            //     AnyColor = true,
+            //     SolidColorOnly = false,
+            //     AllowFullOpen = true,
+            //     FullOpen = true
+            // };
+            // 
+            // if (def is SKColor d) {
+            //     dialog.Color = Color.FromArgb(d.Alpha, d.Red, d.Green, d.Blue);
+            // }
+            // 
+            // if (dialog.ShowDialog() == DialogResult.OK) {
+            //     Color c = dialog.Color;
+            //     return new SKColor(c.R, c.G, c.B, c.A);
+            // }
+            // 
+            // return null;
+        }
     }
 }
